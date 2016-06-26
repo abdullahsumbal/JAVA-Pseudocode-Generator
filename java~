b59dd7c157141b2a4c3a79d0cd8c99f5c6b225fb
abdullahsumbal <abdullahsumbal@gmail.com>
2016-06-26 16:26:40 -0400

    first commit

---
+++ java~
@@ -0,0 +1,12 @@
+// this is the tester code
+
+// void init
+
+public void yolo (int a , int b){
+
+	int a = 10;
+	char a = 'a';
+
+
+
+}
---
+++ t.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python
+
+import re
+import random
+import sys
+import copy
+
+# - variables
+round_brackets_stack = []
+curly_brackets_stack = []
+comments = ""
+
+# - data types
+dataTypes = ['int', 'char' , 'boolean', 'String','long','void']
+
+# - read java code
+input = open('java','r')
+allJavaCode = input.read()
+
+# - remove comments
+for i in range(0,len(allJavaCode)):
+    if i <len(allJavaCode)-1   and allJavaCode[i] == "/" and allJavaCode[i+1] == "/":
+        while allJavaCode[i] != "\n":
+            comments =  comments + allJavaCode[i] 
+            i = i +1
+
+        allJavaCode =  allJavaCode.replace(comments,'')
+    comments = ""
+
+print allJavaCode
+# - check for the opening and closing brackets
+for i in range(0,len(allJavaCode)):
+    if allJavaCode[i] == '(':
+        round_brackets_stack.append("(")
+    if allJavaCode[i] == ')':
+        try: 
+            round_brackets_stack.pop()
+        except:
+            print "missing a round bracket "
+            sys.exit()
+
+    if allJavaCode[i] == '{':
+        curly_brackets_stack.append("{")
+    if allJavaCode[i] == '}':
+        try:
+            curly_brackets_stack.pop()
+        except:
+            print "curly bracket missing"
+            sys.exit()
+
+
+if len(round_brackets_stack) == 0 and len(curly_brackets_stack) == 0:
+    print "your brackets are good"
+else:
+    print "check your brackets"
+    sys.exit()
+
+
+# - find the name of method
+methodDecalration = re.findall(r'.*[public,private]?\s*[static]?\s*\w+\s*[\[]?\s*[\]]?\s+[a-zA-Z,\_,\$]{1}\w*\s*\(.*\)',allJavaCode)
+if len(methodDecalration) > 1:
+    print "please make sure there is only one method"
+    sys.exit()
+elif len (methodDecalration)==1:
+    open_round_bracket_index = methodDecalration[0].index("(")
+    for i in range(0,len(methodDecalration[0])):
+        if methodDecalration[0][i] == " " and i < open_round_bracket_index-1:
+            if " " not in methodDecalration[0][i:open_round_bracket_index].strip() :
+                space_index = i
+    method_name = methodDecalration[0][space_index:open_round_bracket_index].strip()
+
+# - find the input of the method
+if len (methodDecalration)==1:
+    close_round_bracket_index = methodDecalration[0].index(")")
+    inputs = methodDecalration[0][open_round_bracket_index+1 :close_round_bracket_index].strip() 
+    raw_input_list = inputs.split(',')
+    input_list = []
+   
+    for input in raw_input_list:
+        input = input.strip()
+        for i in range(0,len(input)):
+            if input[i] == " ":
+                if " " not in input[i+1:len(input)]:
+                    space_index = i
+        input_list.append(input[space_index:len(input)].strip())
+
+# - the convert everything inside the method
+
+def convert(allJavaCode, spaces,method_content):
+    method_start = allJavaCode.index("{")
+    index = 1
+    while allJavaCode[len(allJavaCode)-index] != "}":
+        index = index + 1
+        method_end = len(allJavaCode) - index
+         
+    raw_method_content = allJavaCode[method_start+1:method_end].strip()
+    inst_list = raw_method_content.split(";")
+
+    
+
+    for inst in inst_list:
+        inst =  inst.strip()
+        
+        # - system.out.print statments
+        if "System.out.print" in inst:
+            print_to_screen = re.findall(r'\".*\"',inst)
+            
+            print_to_screen[0] = "Print "+ print_to_screen[0]
+            method_content.append(print_to_screen[0])
+
+        # - int statments
+        if re.split(r'\s',inst)[0] == "int" and "[]" not in inst:
+            int_variable_name = re.split(r'\s',inst)[1]
+            if "=" in inst:
+                int_value = re.split(r'=',inst)[1].strip()
+                method_content.append(int_variable_name+u" \u2190 "+int_value)
+                
+        # - char statement
+        if re.split(r'\s',inst)[0] == "char" and "[]" not in inst:
+            char_variable_name = re.split(r'\s',inst)[1]
+            if "=" in inst:
+                char_value = re.split(r'=',inst)[1].strip()
+                method_content.append(char_variable_name+u" \u2190 "+char_value)
+
+        # - 
+
+method_content = []
+convert(allJavaCode,0,method_content)
+
+
+        
+
+print "----------------------------------------------------"
+print "Algothrim:" + method_name
+print "----------------------------------------------------"
+print "INPUT: " + ", ".join(input_list)
+print "OUPUT: "
+
+print "----------------------------------------------------\n"
+for inst in method_content:
+    print inst
+
